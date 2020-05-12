957dff0b759e3c9cd2f39a07ed48011f38f05768
H. Can Yıldırım <caanyildirims@gmail.com>
2020-05-13 01:38:32 +0300

    :rocket: make add transaction button functional.

--- src/main/java/controllers/DashboardController.java
+++ src/main/java/controllers/DashboardController.java
@@ -2,16 +2,18 @@ package controllers;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
+import javafx.stage.Window;
 import models.Transaction;
 import models.UserSession;
 import services.TransactionService;
+import utilities.AlertHelper;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -25,20 +27,76 @@ public class DashboardController implements Initializable {
     private TableView<Transaction> tableView;
 
     @FXML
+    private Label lblUser;
+
+    @FXML
     private ComboBox<Transaction.Type> typeOptions;
 
     @FXML
-    private Label lblUser;
+    private TextField descriptionField;
+
+    @FXML
+    private TextField amountField;
+
+    @FXML
+    private Button addButton;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         try {
-            transactions = FXCollections.observableArrayList(transactionService.getByUserId(UserSession.getInstance().getUser().getId()));
-            tableView.getItems().setAll(transactions);
-            typeOptions.getItems().setAll(Transaction.Type.values());
-            lblUser.setText(String.format("Logged in as: %s", UserSession.getInstance().getUser().getUsername()));
+            populateTableItems();
+            populateTransactionTypeOptions();
+            setLoggedInLabelText();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    @FXML
+    public void addTransaction(ActionEvent event) throws IOException {
+        Window owner = addButton.getScene().getWindow();
+
+        if (descriptionField.getText().isEmpty()) {
+            AlertHelper.showAlert(Alert.AlertType.ERROR, owner, "Form Error!", "Please enter a description");
+            return;
+        }
+
+        if (amountField.getText().isEmpty()) {
+            AlertHelper.showAlert(Alert.AlertType.ERROR, owner, "Form Error!", "Please enter an amount");
+            return;
+        }
+
+        if (typeOptions.getValue() == null) {
+            AlertHelper.showAlert(Alert.AlertType.ERROR, owner, "Form Error!", "Select your transaction type");
+            return;
+        }
+
+        String description = descriptionField.getText();
+        BigDecimal amount = new BigDecimal(amountField.getText());
+        Transaction.Type type = typeOptions.getValue();
+
+        transactionService.create(description, amount, type, UserSession.getInstance().getUser().getId());
+
+        populateTableItems();
+        clearInputs();
+    }
+
+    private void populateTableItems() throws IOException {
+        transactions = FXCollections.observableArrayList(transactionService.getByUserId(UserSession.getInstance().getUser().getId()));
+        tableView.getItems().setAll(transactions);
+    }
+
+    private void populateTransactionTypeOptions() {
+        typeOptions.getItems().setAll(Transaction.Type.values());
+    }
+
+    private void setLoggedInLabelText() {
+        lblUser.setText(String.format("Logged in as: %s", UserSession.getInstance().getUser().getUsername()));
+    }
+
+    private void clearInputs() {
+        descriptionField.clear();
+        amountField.clear();
+        typeOptions.getSelectionModel().clearSelection();
     }
 }
